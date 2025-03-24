82c4a9121022d71c1a19ee1f8b635d9e7f2b6f51
ahmedElsharnouby <ahmedelsharnouby29@gmail.com>
2025-03-24 08:46:36 +0200

    Update: change LaunchSettion.json

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -5,9 +5,13 @@ namespace Catalog.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Add servies to container
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
+            // Configure the http request pipeline
 
             app.Run();
         }
